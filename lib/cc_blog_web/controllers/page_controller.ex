887476f6b705f547e524fa4cc9caaedf6b37cbe8
Chris Caragianis <chriscaragianis@gmail.com>
2018-03-25 23:27:44 -0400

    cartoons and layout

--- lib/cc_blog_web/controllers/page_controller.ex
+++ lib/cc_blog_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule CcBlogWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def politics(conn, _params) do
+    render conn, "politics.html"
+  end
 end
--- lib/cc_blog_web/router.ex
+++ lib/cc_blog_web/router.ex
@@ -17,6 +17,7 @@ defmodule CcBlogWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/politics", PageController, :politics
   end
 
   # Other scopes may use custom stacks.
